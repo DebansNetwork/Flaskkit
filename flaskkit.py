f3a3c4feb6dc3d29312924c82b699e547f0ff914
Nicolas FAVRE <norfair00@gmail.com>
2015-02-11 15:48:01 +0100

    Add flask-PyMongo

--- flaskkit.py
+++ flaskkit.py
@@ -1,6 +1,13 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
+from flask.ext.pymongo import PyMongo
+
 app = Flask(__name__)
+mongo = PyMongo(app)
+
+app.config['MONGO_HOST'] = '127.0.0.1'
+app.config['MONGO_PORT'] = 27017
+app.config['MONGO_DBNAME'] = 'flasktest1'
+mongo = PyMongo(app, config_prefix='MONGO')
 
 @app.route('/')
 def index():
